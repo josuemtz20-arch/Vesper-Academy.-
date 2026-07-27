cfefbae848eedb797bd095d7129d6d6da674f55d
josuemtz20-arch <josuemtz20-arch@users.noreply.github.com>
2026-07-26 19:16:16 -0600

    sw v89: el mapa por tema cambia leccion.html

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    ============================================================ */
 "use strict";
 
-var CACHE_VERSION = "vesper-v88";
+var CACHE_VERSION = "vesper-v89";
 var CORE = [
   /* Vesper Engine shell */
   "vesper_engine.html",
